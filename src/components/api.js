072dc33f3bfd19705c61b82d54205773281b995c
Dumitru432 <89398647+Dumitru432@users.noreply.github.com>
2023-08-15 09:21:37 +0300

    weather app in react

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '2bf8a2ecb7mshf984f1bf1a87c6dp18e79djsnb3a461afda89',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '5112f77230d2a0f65e77a41849a996a6';
